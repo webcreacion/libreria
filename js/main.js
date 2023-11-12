b945682281d219fe8271a8047bbb6063a00a84df
WebCreación <72196135+webcreacion@users.noreply.github.com>
2023-11-12 13:05:02 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,81 +1,46 @@
-import {Libro, libros, buscarLibroPorAutor} from './libros.js';
-const tablaLibros = document.getElementById('tablaLibros');
-const filtroAutorEvent = document.getElementById('filtroAutorEvent');
-const filtroAutor = document.getElementById('filtroAutor');
-const restaurar = document.getElementById('restaurar');
-const portada = document.getElementById('portada');
+const tabla = document.getElementById('tabla');
+let libros = [];
+
+//Importamos el objeto JSON
+fetch('./js/libros.json')
+	.then(response => response.json())
+	.then(data=> {
+		libros = data;
+		renderizarTabla(libros);		
+	})
+	.catch(error => {
+		  console.error('Error al cargar y analizar el archivo JSON:', error);
+});
 
 
-console.log(libros[0].titulo)
-
-let saludo = "Hola";
-
-
-
-function todosLosLibros(){
-    tablaLibros.innerHTML = '';
-    libros.forEach((book)=>{
-        tablaLibros.insertAdjacentHTML('beforeend', `
-            <tr>
-                <td>${book.titulo}</td>
-                <td>${book.autor}</td>
-            </tr>`)
-    });
-
+//Renderiza la tabla
+function renderizarTabla(libros){
+	tabla.innerHTML = `
+			<theader>
+				<tr>
+					<th>Título</th>
+					<th>Autor</th>
+					<th>Sinopsis</th>
+					<th>Portada</th>
+				</th>
+			</theader>
+			<tbody>
+		`
+		libros.forEach((element, index)=>{
+			tabla.insertAdjacentHTML('beforeend', 
+				`
+					<tr>
+						<td>${element.titulo}</td>
+						<td>${element.autor}</td>
+						<td>${element.sinopsis}</td>
+						<td><img src=${element.portada} alt="portada del libro ${element.titulo}" height="100"></td>
+					</th>
+					
+				`
+			)
+		});
+		tabla.insertAdjacentHTML('beforeend', '</tbody>');
 }
-let filtro = false;
-let librosFiltrados;
-todosLosLibros();
-
-//Mostrar imagen
-const filas = document.querySelectorAll('table tr');
-filas.forEach(function(fila, index){
-    fila.addEventListener('mouseover', function(){
-        portada.style.display = "block";
-        // portada.src = ;
-        if(filtro){
-            portada.src = librosFiltrados[index-1].url;
-        }else {
-            portada.src = libros[index-1].url;
-        }
-       
-    })
-})
-
-
-
-filtroAutorEvent.addEventListener('click', ()=>{
-    filtro = !filtro;
-    tablaLibros.innerHTML = '';
-    librosFiltrados = buscarLibroPorAutor(filtroAutor.value, libros);
-   
-
-    librosFiltrados.forEach((book)=>{
-        tablaLibros.insertAdjacentHTML('beforeend', `
-            <tr>
-                <td>${book.titulo}</td>
-                <td>${book.autor}</td>
-            </tr>`)
-    });
-
-   
-    if(tablaLibros.innerHTML == '' ){
-        alert('No se encuentran libros en el filtro')
-        todosLosLibros();
-    
-    
-    };
-});
-
-//Botón quita filtros
-restaurar.addEventListener('click', function(){
-    todosLosLibros();
-    filtro= !filtro;
-   
-});
-
-
-
 
 
 
